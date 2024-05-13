e777fc454f3f5b5bc8beb08ead691ca7eccd7502
Franck Fernandez <franck.fernandez@gmail.com>
2024-05-13 22:02:55 +0200

    Create room page

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -1,3 +1,60 @@
+import CopyClipboard from '@/components/CopyClipboard';
+import { Badge } from '@/components/ui/badge';
+import { Input } from '@/components/ui/input';
+
+const CATEGORIES = [
+  'Animal',
+  'Pays',
+  'Métier',
+  'Prénom',
+  'Fruit/légume',
+  'Ville',
+  'Marque',
+  'Objet',
+  'Célébrité',
+  'Sport',
+  'Fleuve ou rivière',
+  'Objet de couleur bleu',
+  'Cadeau pour une épouse',
+  'Plat',
+  'Partie du corps',
+  'Mot au hasard',
+  'Fromage',
+  'Qui est vert',
+  'Maladies',
+  'Pokémon',
+  'Marque alimentaire',
+  'Instrument de musique',
+  'Objet du quotidien',
+  'Objet éléctronique',
+  'Cadeau pour un mari',
+  'Types de musique',
+  'Super héro',
+  'Cadeau pour une petite amie',
+  'Animaux marins',
+];
+
 export default function Room({ params: { id } }: { params: { id: string } }) {
-  return <main>{id}</main>;
+  return (
+    <main className="px-72 pt-5 space-y-8">
+      <h1>Nouvelle partie</h1>
+
+      <section>
+        <h2>Catégories</h2>
+        <div className="flex flex-wrap gap-2">
+          {CATEGORIES.map((c, idx) => (
+            <Badge key={idx}>{c}</Badge>
+          ))}
+        </div>
+      </section>
+
+      <section>
+        <h2>Lien de la partie</h2>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+          <Input type="text" value={id} disabled />
+          <CopyClipboard text={id} />
+        </div>
+      </section>
+    </main>
+  );
 }
