d4a62299404f6a536f948995a12760ab0e688fa6
Franck Fernandez <franck.fernandez@gmail.com>
2024-05-14 11:09:31 +0200

    Remove Label from new room form

--- components/RoomForm.tsx
+++ components/RoomForm.tsx
@@ -1,25 +1,18 @@
 import { createRoom } from '@/app/actions';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
-import { Label } from './ui/label';
 import { cn } from '@/lib/utils';
 
 export default function RoomForm({ className }: { className?: string }) {
   return (
     <section className="border-l pl-10">
       <h2>Create room</h2>
-      <form action={createRoom} className={cn('space-y-2', className)}>
-        <Label htmlFor="name">Name</Label>
-        <div className="flex w-full max-w-sm items-center space-x-2">
-          <Input
-            name="name"
-            type="text"
-            placeholder="Room name"
-            defaultValue="Room #1"
-            required
-          />
-          <Button type="submit">Create</Button>
-        </div>
+      <form
+        action={createRoom}
+        className={cn('flex w-full max-w-sm items-center space-x-2', className)}
+      >
+        <Input name="name" type="text" placeholder="Room name" required />
+        <Button type="submit">Create</Button>
       </form>
     </section>
   );
