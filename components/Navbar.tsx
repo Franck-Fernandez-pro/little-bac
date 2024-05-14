a112826f194f605b71fee8b997598587ebb9c33c
Franck Fernandez <franck.fernandez@gmail.com>
2024-05-14 20:04:07 +0200

    Minor fix/config

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { ModeToggle } from './ThemeButton';
 
-export default function Navbar({}: {}) {
+export default function Navbar() {
   return (
     <nav className="w-full flex justify-between p-5">
       <Link href="/" className="font-bold">
