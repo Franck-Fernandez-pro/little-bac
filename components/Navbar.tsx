76677f57390bc5cb8d02162be0a9f7ea8b591409
Franck Fernandez <franck.fernandez@gmail.com>
2024-05-14 11:24:50 +0200

    Create Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import { ModeToggle } from './ThemeButton';
+
+export default function Navbar({}: {}) {
+  return (
+    <nav className="w-full flex justify-between p-5">
+      <Link href="/" className="font-bold">
+        LittleBac
+      </Link>
+      <ModeToggle />
+    </nav>
+  );
+}
