b676295290f80da858818e574534bf70346914dc
Franck Fernandez <franck.fernandez@gmail.com>
2024-05-14 10:40:01 +0200

    Add Skeleton to Rooms

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Rooms from '@/components/Rooms';
 export default function Home() {
   return (
     <main className="flex justify-between w-full mt-5 px-10">
-      <Rooms className="" />
+      <Rooms />
       <RoomForm className="min-w-72" />
     </main>
   );
--- components/Rooms.tsx
+++ components/Rooms.tsx
@@ -5,22 +5,26 @@ import { useQuery } from 'convex/react';
 import { Button } from './ui/button';
 import { Card, CardTitle, CardContent } from './ui/card';
 import { Doc } from '@/convex/_generated/dataModel';
-import { cn } from '@/lib/utils';
 import Link from 'next/link';
+import { Skeleton } from './ui/skeleton';
 
 export default function Rooms({ className }: { className?: string }) {
   const rooms = useQuery(api.rooms.get);
-
-  if (rooms === undefined) return <div>Loading...</div>;
-  if (rooms.length === 0) return <div>No room</div>;
 
   return (
     <section className={className}>
       <h2>Rooms</h2>
       <div className="flex flex-wrap gap-4">
-        {rooms.map((room) => (
-          <RoomCard key={room._id} room={room} />
-        ))}
+        {rooms === undefined && [
+          <SkeletonCard />,
+          <SkeletonCard />,
+          <SkeletonCard />,
+          <SkeletonCard />,
+        ]}
+        {rooms !== undefined && rooms.length === 0 && <div>No room</div>}
+        {rooms !== undefined &&
+          rooms.length > 0 &&
+          rooms.map((room) => <RoomCard key={room._id} room={room} />)}
       </div>
     </section>
   );
@@ -33,6 +37,15 @@ const RoomCard = ({ room: { _id, name } }: { room: Doc<'rooms'> }) => (
       <Button asChild>
         <Link href={`/room/${_id}`}>Join</Link>
       </Button>
+    </CardContent>
+  </Card>
+);
+
+const SkeletonCard = () => (
+  <Card className="w-[350px] h-[88px]">
+    <CardContent className="pt-6 flex justify-between items-center h-full">
+      <Skeleton className="h-6 w-[160px] rounded-lg" />
+      <Skeleton className="h-6 w-[50px] rounded-lg" />
     </CardContent>
   </Card>
 );
