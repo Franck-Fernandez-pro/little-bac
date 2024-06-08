a86b9e3446c03bdeb9c25a5f24cfdcd7e6b05cd2
Franck <franck.fernandez@gmail.com>
2024-06-08 11:54:32 +0200

    Remove multiple categories

--- app/room/[id]/page.tsx
+++ app/room/[id]/page.tsx
@@ -5,37 +5,22 @@ import { Id } from '@/convex/_generated/dataModel';
 import { useQuery } from 'convex/react';
 import WaitingRoom from './_components/Waiting';
 import Ended from './_components/Ended';
+import Running from './_components/Running';
 
 const CATEGORIES = [
   'Animal',
   'Pays',
   'Métier',
-  'Prénom',
   'Fruit/légume',
   'Ville',
   'Marque',
   'Objet',
   'Célébrité',
   'Sport',
-  'Fleuve ou rivière',
-  'Objet de couleur bleu',
-  'Cadeau pour une épouse',
-  'Plat',
   'Partie du corps',
-  'Mot au hasard',
-  'Fromage',
-  'Qui est vert',
-  'Maladies',
-  'Pokémon',
-  'Marque alimentaire',
   'Instrument de musique',
   'Objet du quotidien',
-  'Objet éléctronique',
-  'Cadeau pour un mari',
-  'Types de musique',
   'Super héro',
-  'Cadeau pour une petite amie',
-  'Animaux marins',
 ];
 
 export default function Room({ params: { id } }: { params: { id: string } }) {
@@ -48,6 +33,7 @@ export default function Room({ params: { id } }: { params: { id: string } }) {
       {room.state === 'waiting' && (
         <WaitingRoom id={id} categories={CATEGORIES} />
       )}
+      {room.state === 'running' && <Running id={id} categories={CATEGORIES} />}
       {room.state === 'ended' && <Ended />}
     </main>
   );
