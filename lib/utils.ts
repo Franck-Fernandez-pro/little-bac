a64ac66e134a03c754eef18da40a458543cebe37
Franck <franck.fernandez@gmail.com>
2024-06-08 20:42:10 +0200

    Improve CATEGORIES const

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+export const CATEGORIES = {
+  animal: 'Animal',
+  country: 'Pays',
+  job: 'Métier',
+  fruit: 'Fruit/légume',
+  city: 'Ville',
+  brand: 'Marque',
+  object: 'Objet',
+  celebrity: 'Célébrité',
+  sport: 'Sport',
+  bodyPart: 'Partie du corps',
+  instrument: 'Instrument de musique',
+  dailyObject: 'Objet du quotidien',
+  superHero: 'Super héro',
+} as const;
+
+export const CATEGORIES_KEYS = Object.keys(CATEGORIES);
+export const CATEGORIES_VALUES = Object.values(CATEGORIES);
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
