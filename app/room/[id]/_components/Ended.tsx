43910780a8ba3de8ac2c8b3e4a756c1684c8184e
Franck <franck.fernandez@gmail.com>
2024-06-09 21:00:26 +0200

    Add UI for displaying results in Ended component

--- app/room/[id]/_components/Ended.tsx
+++ app/room/[id]/_components/Ended.tsx
@@ -1,11 +1,24 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Doc } from '@/convex/_generated/dataModel';
+import { Label } from '@/components/ui/label';
+import { api } from '@/convex/_generated/api';
+import { CATEGORIES_ENTRIES } from '@/lib/utils';
+import { FunctionReturnType } from 'convex/server';
 import { useRouter } from 'next/navigation';
 
-export default function Ended({ room }: { room: Doc<'rooms'> }) {
+export default function Ended({
+  room,
+}: {
+  room: Exclude<FunctionReturnType<typeof api.room.get>, null>;
+}) {
   const router = useRouter();
+  const user = room.users?.reduce((acc: Record<string, string>, user) => {
+    if (user) {
+      acc[user._id] = user.name;
+    }
+    return acc;
+  }, {});
 
   async function back() {
     router.push('/');
@@ -14,6 +27,21 @@ export default function Ended({ room }: { room: Doc<'rooms'> }) {
   return (
     <>
       <h1>Partie terminée</h1>
+
+      <section className="flex w-full flex-col space-y-5 ml-2">
+        {CATEGORIES_ENTRIES.map(([key, value]) => (
+          <div key={key}>
+            <Label>{value}</Label>
+            {room.results?.map((result) => (
+              <div key={`${key}_${result.userId}`} className="ml-5">
+                <span className="mr-1">{user[result.userId as string]} :</span>
+                {/* @ts-ignore */}
+                <span>{result.response[key]}</span>
+              </div>
+            ))}
+          </div>
+        ))}
+      </section>
 
       <Button onClick={back}>Retourner à l'accueil</Button>
     </>
