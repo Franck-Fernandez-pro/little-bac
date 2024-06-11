2afb1c8f7f9c3e60c3b9078834fccbcc09f79177
Franck <franck.fernandez@gmail.com>
2024-06-11 15:51:32 +0200

    Update Navbar component to center align items

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from './ThemeButton';
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex justify-between p-5">
+    <nav className="w-full flex justify-between items-center p-5">
       <Link href="/" className="font-bold">
         LittleBac
       </Link>
